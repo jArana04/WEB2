63c1da9ce1aedb693ab3802a84d267b6844e7d5a
jArana04 <126832871+jArana04@users.noreply.github.com>
2023-06-08 23:25:21 -0300

    cambios

---
+++ myscript.js
@@ -0,0 +1,46 @@
+
+//let numeros= [15,25,25,65]
+//array comienza en 0, em la primera posicion
+//el array se declara con const y se escribe todo en mayuscula , const ARRAY = [11,"JUAN"]
+//array puede modificar const. 
+
+//metodos de array:
+//1= push, agrega un valor al array y le tenemos que indicar que queremos agregar
+//2= pop, quita ultimo elemento aca no tenemos que indicar nada xq es el ultimo
+//3= shift, elimina el primer elemento
+//4= unshift, agrega un elemento, debemos indicar que agregar 
+
+
+//let profesor= { ---------> esto quiere decir objeto literal
+//nombre="esteban"
+//edad=29+1
+//consol.log(mejor.profesor.nombre) printea esteban}
+
+//formas de declarar variables:
+
+//1))
+//function saludar (){
+    //console.log("hola mundo")
+//} 
+//saludar() ----->(aca lo que estamos haceindo es llamar a la funcion para que se printee)
+
+
+//2))
+//function (){
+    //let saludar = finction
+
+//}
+//en este tipo de fromas de declarar funciones no se puede printear antes de que se declare 
+//como se podia en el anterior metodo
+
+//3)
+//const materials = [
+    //'Hydrogen',
+    //'Helium',
+    //'Lithium',
+    //'Beryllium'
+//];
+  
+//console.log(materials.map(material => material.length));
+// Expected output: Array [8, 6, 7, 9]
+
