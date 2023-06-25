4389bcac72baa142f9d383f95ed4f335d95d43ad
jArana04 <126832871+jArana04@users.noreply.github.com>
2023-06-24 23:48:09 -0300

    contador finalizado con algunos problemas

--- myscript.js
+++ myscript.js
@@ -52,4 +52,29 @@
 //no aplicad for a nuestras paginas
 //hacer una cuenta atras y calcular segun el precio en dolar blue convertirlo a peso argentino
 
+const diaa = document.getElementById('dia');
+const horaa =document.getElementById('hora');
+const minutoo= document.getElementById('minuto');
+const segundoos= document.getElementById('segundos');
+const finalMessagee= document.querySelector('.final-sms');
 
+
+const countdownDate= new Date('7 25, 2023 10:28:00').getTime();
+
+let interval= setInterval(function(){
+    const now= new Date().getTime();
+    let distance = countdownDate - now;
+
+    let dia= Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hora= Math.floor((distance % (1000 * 60 * 60 * 24 )) / (1000 * 60 * 60));
+    let minuto= Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let segundos= Math.floor((distance % (1000 * 60 )) / (1000));
+    diaa.innerHTML= dia;
+    horaa.innerHTML= hora;
+    minutoo.innerHTML= minuto;
+    segundoos.innerHTML= segundos; 
+
+    if (distance<0){
+        clearInterval(interval);
+    }
+}, 1000);
